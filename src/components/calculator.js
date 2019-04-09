cb745ce2e22e04701ed4e0b5dc066b7e8912cf49
Andrew Park <13parksangjoon@gmail.com>
2019-04-08 17:44:34 -0700

    initial commit

---
+++ src/components/calculator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../style/calculator.css';
+
+export default class Calculator extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            display: ''
+        }
+    }
+    render() {
+        return (
+            <div className = "Calculator">
+                
+            </div>
+        );
+    }
+}
